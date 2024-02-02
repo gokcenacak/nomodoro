cca6f6d585f12672d56180bd51a16f645cc07d1e
Gökçe Nacak <gokce.erer@gmail.com>
2024-02-02 15:41:21 +0100

    Initial Commit

---
+++ nomodoro/nomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  nomodoroApp.swift
+//  nomodoro
+//
+//  Created by G on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct nomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
